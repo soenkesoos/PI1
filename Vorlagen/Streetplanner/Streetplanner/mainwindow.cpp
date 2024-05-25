e7cec558bdf54d5a24d9febd199aac5153f805cf
fladenbrot133 <sunshinecraft@s-z.us>
2024-05-25 19:01:44 +0200

    Beginning of Versuch 09

---
+++ Vorlagen/Streetplanner/Streetplanner/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QPushButton>
+#include <QLineEdit>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
